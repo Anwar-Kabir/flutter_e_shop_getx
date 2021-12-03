2996c8597909d733707cedd8c2a4164ad5992828
Anwar Kabir <munnarahman1994@gmail.com>
2021-12-03 21:55:29 +0600

    Initial commit

---
+++ lib/controllers/cart_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_e_shop_getx/models/product.dart';
+import 'package:get/state_manager.dart';
+
+
+class CartController extends GetxController {
+  var cartItems = <Product>[].obs;
+  int get count => cartItems.length;
+  double get totalPrice => cartItems.fold(0, (sum, item) => sum + item.price);
+
+  addToCart(Product product) {
+    cartItems.add(product);
+  }
+}
